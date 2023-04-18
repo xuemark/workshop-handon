62fcd8d8b339d7ce45ae122ac63e8cd9e72043a3
Xue <markxue@08f8bc7242a8.ant.amazon.com>
2023-04-18 22:28:25 +0800

    v21

---
+++ create_forecast.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+# ENV
+export AWS_DEFAULT_REGION=ap-northeast-1
+export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+export AWS_BUCKET=forecast-20230416-${AWS_ACCOUNT_ID}
+export AWS_FORECAST_ROLE=forecast-execrole
+export FORECAST_DATASET_1=testdata
+# create Forecast exec role
+cat <<EOF > role-inline-policy.json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject",
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::*"
+            ]
+        }
+    ]
+}
+EOF
+cat <<EOF > role-trust-policy.json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "forecast.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+aws iam create-role --role-name ${AWS_FORECAST_ROLE} --assume-role-policy-document file://role-trust-policy.json
+aws iam put-role-policy --role-name ${AWS_FORECAST_ROLE} --policy-name policy --policy-document file://role-inline-policy.json
+# create Dataset
+echo "[`date +%Y/%m/%d-%H:%M:%S`] - Start Tasks - ${FORECAST_DATASET_1}"
+aws forecast create-dataset-group --dataset-group-name ${FORECAST_DATASET_1} --domain CUSTOM
+aws forecast create-dataset --dataset-name ${FORECAST_DATASET_1} --domain CUSTOM --dataset-type TARGET_TIME_SERIES --data-frequency 1H \
+    --schema 'Attributes=[{AttributeName=item_id,AttributeType=string},{AttributeName=timestamp,AttributeType=timestamp},{AttributeName=target_value,AttributeType=float}]'
+aws forecast update-dataset-group --dataset-group-arn arn:aws:forecast:${AWS_DEFAULT_REGION}:${AWS_ACCOUNT_ID}:dataset-group/${FORECAST_DATASET_1} --dataset-arns arn:aws:forecast:${AWS_DEFAULT_REGION}:${AWS_ACCOUNT_ID}:dataset/${FORECAST_DATASET_1}
+aws forecast create-dataset-import-job --dataset-import-job-name ${FORECAST_DATASET_1} --timestamp-format 'yyyy-MM-dd HH:mm:ss' \
+    --no-use-geolocation-for-time-zone --format CSV \
+    --dataset-arn arn:aws:forecast:ap-northeast-1:${AWS_ACCOUNT_ID}:dataset/${FORECAST_DATASET_1} \
+    --data-source 'S3Config={Path='s3://${AWS_BUCKET}/NYC_Taxi_TimeSeriesDataset.csv',RoleArn='arn:aws:iam::${AWS_ACCOUNT_ID}:role/${AWS_FORECAST_ROLE}'}'
+while  [ "$(aws forecast describe-dataset-import-job --dataset-import-job-arn arn:aws:forecast:${AWS_DEFAULT_REGION}:${AWS_ACCOUNT_ID}:dataset-import-job/${FORECAST_DATASET_1}/${FORECAST_DATASET_1}  --query Status --output text)" != "ACTIVE" ];
+do
+    echo "[`date +%Y/%m/%d-%H:%M:%S`] - $(aws forecast describe-dataset-import-job --dataset-import-job-arn arn:aws:forecast:${AWS_DEFAULT_REGION}:${AWS_ACCOUNT_ID}:dataset-import-job/${FORECAST_DATASET_1}/${FORECAST_DATASET_1}  --query Status --output text)";
+    sleep 10;
+done
+echo "[`date +%Y/%m/%d-%H:%M:%S`] - Dataset Import Job Completed"
+# create auto predictor
+cat <<EOF > data-config.json
+{
+    "DatasetGroupArn": "arn:aws:forecast:${AWS_DEFAULT_REGION}:${AWS_ACCOUNT_ID}:dataset-group/${FORECAST_DATASET_1}",
+    "AttributeConfigs": [
+        {
+            "AttributeName": "target_value",
+            "Transformations": {
+                "aggregation": "sum",
+                "backfill": "zero",
+                "frontfill": "none",
+                "middlefill": "zero"
+            }
+        }
+    ],
+    "AdditionalDatasets": [
+        {
+            "Name": "holiday",
+            "Configuration": {
+                "CountryCode": [
+                    "US"
+                ]
+            }
+        }
+    ]
+}
+EOF
+export FORECAST_PREDICTOR_ARN=$(aws forecast create-auto-predictor --predictor-name ${FORECAST_DATASET_1} --explain-predictor  --forecast-types "0.10" "0.50" "0.9" \
+    --forecast-frequency 1H --forecast-horizon 24 \
+    --data-config file://data-config.json \
+    --query PredictorArn --output text)
+echo "FORECAST_PREDICTOR_ARN: ${FORECAST_PREDICTOR_ARN}"
+while  [ "$(aws forecast describe-auto-predictor --predictor-arn ${FORECAST_PREDICTOR_ARN} --query Status --output text)" != "ACTIVE" ];
+do
+    echo "[`date +%Y/%m/%d-%H:%M:%S`] - $(aws forecast describe-auto-predictor --predictor-arn ${FORECAST_PREDICTOR_ARN} --query Status --output text)";
+    sleep 30;
+done
+echo "[`date +%Y/%m/%d-%H:%M:%S`] - Create Auto Predictor Completed"
+# create forecast
+aws forecast create-forecast --forecast-name ${FORECAST_DATASET_1} --predictor-arn  ${FORECAST_PREDICTOR_ARN} --forecast-types "0.10" "0.50" "0.9" 
+while  [ "$(aws forecast describe-forecast --forecast-arn arn:aws:forecast:${AWS_DEFAULT_REGION}:${AWS_ACCOUNT_ID}:forecast/${FORECAST_DATASET_1}  --query Status --output text)" != "ACTIVE" ];
+do
+    echo "[`date +%Y/%m/%d-%H:%M:%S`] - $(aws forecast describe-forecast --forecast-arn arn:aws:forecast:${AWS_DEFAULT_REGION}:${AWS_ACCOUNT_ID}:forecast/${FORECAST_DATASET_1}  --query Status --output text)";
+    sleep 30;
+done
+echo "[`date +%Y/%m/%d-%H:%M:%S`] - Create Forecast Completed"
+# create explainability
+cat <<EOF > itemids.csv
+item_id
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+EOF
+aws s3 cp itemids.csv s3://${AWS_BUCKET}/
+aws forecast create-explainability --explainability-name ${FORECAST_DATASET_1}_forecast \
+--resource-arn arn:aws:forecast:${AWS_DEFAULT_REGION}:${AWS_ACCOUNT_ID}:forecast/${FORECAST_DATASET_1} \
+--data-source 'S3Config={Path='s3://${AWS_BUCKET}/itemids.csv',RoleArn='arn:aws:iam::${AWS_ACCOUNT_ID}:role/${AWS_FORECAST_ROLE}'}' \
+--schema 'Attributes={AttributeName=item_id,AttributeType=string}' --enable-visualization \
+--explainability-config 'TimeSeriesGranularity=SPECIFIC,TimePointGranularity=ALL'
+while  [ "$(aws forecast describe-explainability --explainability-arn arn:aws:forecast:${AWS_DEFAULT_REGION}:${AWS_ACCOUNT_ID}:explainability/${FORECAST_DATASET_1}_forecast --query Status --output text)" != "ACTIVE" ];
+do
+    echo "[`date +%Y/%m/%d-%H:%M:%S`] - $(aws forecast describe-explainability --explainability-arn arn:aws:forecast:${AWS_DEFAULT_REGION}:${AWS_ACCOUNT_ID}:explainability/${FORECAST_DATASET_1}_forecast --query Status --output text)";
+    sleep 30;
+done
+echo "[`date +%Y/%m/%d-%H:%M:%S`] - Create Forecast Explainability Completed"
+# create Forecast Export Job
+aws forecast create-forecast-export-job --forecast-export-job-name ${FORECAST_DATASET_1} \
+--forecast-arn arn:aws:forecast:${AWS_DEFAULT_REGION}:${AWS_ACCOUNT_ID}:forecast/${FORECAST_DATASET_1} \
+--destination 'S3Config={Path='s3://${AWS_BUCKET}/export/',RoleArn='arn:aws:iam::${AWS_ACCOUNT_ID}:role/${AWS_FORECAST_ROLE}'}'
+while  [ "$(aws forecast describe-forecast-export-job --forecast-export-job-arn arn:aws:forecast:${AWS_DEFAULT_REGION}:${AWS_ACCOUNT_ID}:forecast-export-job/${FORECAST_DATASET_1}/${FORECAST_DATASET_1} --query Status --output text)" != "ACTIVE" ];
+do
+    echo "[`date +%Y/%m/%d-%H:%M:%S`] - $(aws forecast describe-forecast-export-job --forecast-export-job-arn arn:aws:forecast:${AWS_DEFAULT_REGION}:${AWS_ACCOUNT_ID}:forecast-export-job/${FORECAST_DATASET_1}/${FORECAST_DATASET_1} --query Status --output text)";
+    sleep 30;
+done
+echo "[`date +%Y/%m/%d-%H:%M:%S`] - Create Forecast Export Job Completed"
